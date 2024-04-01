1c697bc93c4a64a8227373d009bb811210640179
victor.debuen <victor.debuen@inverence.com>
2024-04-01 11:54:19 +0000

    New ExtSummary function

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,6 +1,19 @@
 .onAttach <- function(libname, pkgname){
   library(DT)
   library(plotly)
+  require(lubridate)
+
+  options(ExtSummary.showRMSE=TRUE)
+  options(ExtSummary.showQtle="ALL")
+  options(ExtSummary.nQtle0=2)
+  options(ExtSummary.nQtlem=3)
+  options(ExtSummary.nQtle1=2)
+  options(ExtSummary.showPoints=TRUE)
+  options(ExtSummary.showRMSE=TRUE)
+  options(ExtSummary.showRelativeStats=TRUE)
+  options(ExtSummary.digits=3)
+  options(ExtSummary.max_discrete_levels=20)
+
   options(InvenceRVisualTools.DT.dom='Blfrtip')
   options(InvenceRVisualTools.DT.minPageLength=10)
   options(InvenceRVisualTools.DT.buttons=c('colvis', 'copy', 'excel'))
