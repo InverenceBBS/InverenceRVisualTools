f40184fbf51211d64bacf5f90cfcf09955969c98
victor.debuen <victor.debuen@inverence.com>
2024-06-07 10:29:40 +0000

    Moving ExtSummary to new package  InverenceRSummary

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,18 +1,7 @@
 .onAttach <- function(libname, pkgname){
   library(DT)
   library(plotly)
-  require(lubridate)
-
-  options(ExtSummary.showRMSE=TRUE)
-  options(ExtSummary.showQtle="ALL")
-  options(ExtSummary.nQtle0=2)
-  options(ExtSummary.nQtlem=3)
-  options(ExtSummary.nQtle1=2)
-  options(ExtSummary.showPoints=TRUE)
-  options(ExtSummary.showRMSE=TRUE)
-  options(ExtSummary.showRelativeStats=TRUE)
-  options(ExtSummary.digits=3)
-  options(ExtSummary.max_discrete_levels=20)
+  library(lubridate)
 
   options(InvenceRVisualTools.DT.dom='Blfrtip')
   options(InvenceRVisualTools.DT.minPageLength=10)
